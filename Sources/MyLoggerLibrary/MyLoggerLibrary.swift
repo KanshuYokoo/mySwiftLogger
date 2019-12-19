df39319e36a3ed26a6f12f4e3c2ed22cce7dc91e
Kanshu Yokoo <kanshu.yokoo@gmail.com>
2019-12-19 16:39:43 +0300

    first commit

--- Sources/MyLoggerLibrary/MyLoggerLibrary.swift
+++ Sources/MyLoggerLibrary/MyLoggerLibrary.swift
@@ -1,3 +1,14 @@
-struct MyLoggerLibrary {
-    var text = "Hello, World!"
+
+import Foundation
+import os.log
+
+class Logger {
+    static func log(message: String = "", type:OSLogType = OSLogType.debug, file: String = #file, function: String = #function,column: Int = #column, line: Int = #line) -> Void {
+        self.log(message, type: type, file: file, function: function,column: column,line: line)
+    }
+    
+    static func log(_ message :String, type:OSLogType = OSLogType.debug, file: String = #file, function: String = #function,column: Int = #column, line: Int = #line) -> Void {
+        let formatMessage = "[\(function) \n in \(file) \n at line:\(line) column:\(column)] \n \(message)"
+        os_log("%{public}@", type:type, formatMessage)
+    }
 }
