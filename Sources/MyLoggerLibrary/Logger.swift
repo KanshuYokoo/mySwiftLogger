1e97427c03e420c6ce41dd70100dd1e7ed98a034
Kanshu Yokoo <kanshu.yokoo@gmail.com>
2019-12-22 20:05:07 +0300

    fix:  make public class

--- Sources/MyLoggerLibrary/Logger.swift
+++ Sources/MyLoggerLibrary/Logger.swift
@@ -3,12 +3,12 @@ import Foundation
 import os.log
 
 @available(iOS 10.0, *)
-class Logger {
-    static func log(message: String = "", type:OSLogType = OSLogType.debug, file: String = #file, function: String = #function,column: Int = #column, line: Int = #line) -> Void {
+public class Logger {
+    
+    public static func log(message: String = "", type:OSLogType = OSLogType.debug, file: String = #file, function: String = #function,column: Int = #column, line: Int = #line) -> Void {
         self.log(message, type: type, file: file, function: function,column: column,line: line)
     }
-    
-    static func log(_ message :String, type:OSLogType = OSLogType.debug, file: String = #file, function: String = #function,column: Int = #column, line: Int = #line) -> Void {
+    public static func log(_ message :String, type:OSLogType = OSLogType.debug, file: String = #file, function: String = #function,column: Int = #column, line: Int = #line) -> Void {
         let formatMessage = "[\(function) \n in \(file) \n at line:\(line) column:\(column)] \n \(message)"
         os_log("%{public}@", type:type, formatMessage)
     }
